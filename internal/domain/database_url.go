c31c80760a708ce5aa5a9829f49f632ae9775757
David Voisin <david.voisin@scalingo.com>
2025-12-17 11:55:54 +0100

    fix: redact some secrets
    Still there are flaws, a user may print directly the redacted field.

--- internal/domain/database_url.go
+++ internal/domain/database_url.go
@@ -1,6 +1,12 @@
 package domain
 
+import "fmt"
+
 type DatabaseURL struct {
 	Name  string
-	Value string
+	Value string // WARNING: contains password.
+}
+
+func (u DatabaseURL) String() string {
+	return fmt.Sprintf("{ Name: %s, Value: %s }", u.Name, Redacted)
 }
--- internal/domain/secret.go
+++ internal/domain/secret.go
@@ -1,8 +1,16 @@
 package domain
 
+import "fmt"
+
+const Redacted = "[REDACTED]"
+
 type Secret struct {
 	Namespace string
 	Name      string
 	Key       string
-	Value     string
+	Value     string // WARNING: contains password.
+}
+
+func (s Secret) String() string {
+	return fmt.Sprintf("{ Namespace: %s, Name: %s, Key: %s, Value: %s }", s.Namespace, s.Name, s.Key, Redacted)
 }
